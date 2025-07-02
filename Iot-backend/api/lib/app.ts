1b549b43570cb49dd6e6f7a9e4e9bb22ce1c7657
Damian Duszak <duszakdamian034@gmail.com>
2025-07-02 12:15:43 +0200

    Start of project init server

---
+++ Iot-backend/api/lib/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import {config} from "./config";
+
+class App {
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+    }
+
+    public listen() {
+        this.app.listen(config.port, () => {
+            console.log("Server is running on port " + config.port);
+        });
+    }
+}
+
+export default App;
