40796f089c199db5356c73537e4dd735d448706e
Damian Duszak <duszakdamian034@gmail.com>
2025-07-17 15:59:05 +0200

    init connect with data base

--- Iot-backend/api/lib/app.ts
+++ Iot-backend/api/lib/app.ts
@@ -6,6 +6,7 @@ import morgan from "morgan";
 import http from "http";
 import cors from "cors";
 import {Server, Socket} from "socket.io";
+import mongoose from "mongoose";
 
 class App {
     public app: express.Application;
@@ -17,6 +18,7 @@ class App {
         this.app = express();
         this.initializeMiddlewares();
         this.initializeControllers(controllers);
+        this.connectToDatabase();
         this.server = http.createServer(this.app);
         this.initializeSocket();
     }
@@ -31,6 +33,35 @@ class App {
         this.app.use(bodyParser.json());
         this.app.use(morgan('dev'));
     }
+
+    private async connectToDatabase(): Promise<void> {
+        try {
+            await mongoose.connect(config.databaseUrl);
+            console.log('Connection with database established');
+        } catch (error) {
+            console.error('Error connecting to MongoDB:', error);
+        }
+
+        mongoose.connection.on('error', (error) => {
+            console.error('MongoDB connection error:', error);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB disconnected');
+        });
+        process.on('SIGINT', async () => {
+            await mongoose.connection.close();
+            console.log('MongoDB connection closed due to app termination');
+            process.exit(0);
+        });
+
+        process.on('SIGTERM', async () => {
+            await mongoose.connection.close();
+            console.log('MongoDB connection closed due to app termination');
+            process.exit(0);
+        });
+    }
+
 
     private initializeSocket(): void {
         this.io = new Server(this.server, {
--- Iot-backend/api/lib/config.ts
+++ Iot-backend/api/lib/config.ts
@@ -1,4 +1,5 @@
 export const config = {
     port: process.env.PORT || 3100,
-    socketPort: process.env.SOCKET_PORT || 3000
+    socketPort: process.env.SOCKET_PORT || 3000,
+    databaseUrl: process.env.DATABASE_URL || 'mongodb+srv://duszakdamian034:7AqxblWyn9L0bIM7@cluster0.hbxi47n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
